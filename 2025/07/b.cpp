48d4da18a8463f79c3bd50ccb74116e041f550b3
花音 <kihiro.0422.max@gmail.com>
2025-07-20 20:58:45 +0900

    cpp

--- 2025/07/b.cpp
+++ 2025/07/b.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <chrono>
 using namespace std;
 
 int main() {
-    string n;
+    // 入出力の高速化
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
     cin >> n;
-
-    if ((n[0] == n[1] && n[1] == n[2]) || (n[1] == n[2] && n[2] == n[3])) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
+    
+    auto start = chrono::high_resolution_clock::now();
+    
+    ofstream fout("output.txt");
+    for (int i = 1; i <= n; i++) {
+        fout << i << ".\n";
     }
+    fout << "finish\n";
+    fout.close();
+    
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> duration = end - start;
+    cout << "Execution time: " << duration.count() << " milliseconds" << endl;
+
     return 0;
 }
