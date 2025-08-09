7023dd1c21eb002c279194fd470a08155b1287c7
花音 <kihiro.0422.max@gmail.com>
2025-08-09 20:17:08 +0900

    atcoder 仮想環境構築1

--- 2025/08/a.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-a = list(map(int, input().split()))
-s = 0
-for i, ai in enumerate(a):
-    for j, aj in enumerate(a):
-        if (i - j) == (ai + aj):
-            s += 1
-print(s)
