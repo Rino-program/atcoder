6bfc79d02135ea8444badd057ecb8f2205051c69
Rino-program <kihiro.0422.max@gmail.com>
2026-09-27 01:00:19 +0900

    ABC477

---
+++ ABC/abc477/maine.py
@@ -0,0 +1,170 @@
+# AtCoder Competition Template v2.3.1 SHORT (PyPy 7.3.20 / Python 3.11)
+# ↑ https://github.com/Rino-program/atcoder/blob/main/.template/main.py &template.py
+# oj test -c 'C:\Rino-program\AtCoder\.venv-pypy311\Scripts\python.exe maina.py' -d input/a
+
+import os, sys
+from collections import deque, defaultdict, Counter
+from itertools import permutations, combinations, accumulate, product, chain
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+import operator
+import heapq
+import math
+import string
+
+# ===== 設定・制限解除 =====
+sys.setrecursionlimit(2*10**6)
+
+# ===== 入力ヘルパ =====
+input = lambda: sys.stdin.readline().rstrip()
+INT = lambda: int(input())
+INT0 = lambda: int(input()) - 1
+MAP = lambda: map(int, input().split())
+MAP0 = lambda: (int(x) - 1 for x in input().split())
+LIST = lambda: list(map(int, input().split()))
+LIST0 = lambda: [int(x) - 1 for x in input().split()]
+TUPLE = lambda: tuple(map(int, input().split()))
+LISTS = lambda n: [list(map(int, input().split())) for _ in range(n)]
+TUPLES = lambda n: [tuple(map(int, input().split())) for _ in range(n)]
+LISTSI = lambda n: [int(input()) for _ in range(n)]
+STR = lambda: input()
+STRS = lambda n: [input() for _ in range(n)]
+CHARS = lambda: list(input())
+CHARSL = lambda n: [list(input()) for _ in range(n)]
+CHARSLI = lambda n: [list(map(int, input())) for _ in range(n)]
+
+# ===== 高速 print =====
+def print(*args, sep=' ', end='\n', file=None, flush=False):
+    file = file or sys.stdout
+    sep = ' ' if sep is None else sep
+    end = '\n' if end is None else end
+    file.write(sep.join(map(str, args)) + end)
+    flush and file.flush()
+
+# ===== 定数 =====
+INF = 10 ** 18
+MOD = 998244353
+# MOD = 10**9 + 7
+
+# ===== 関数短縮 =====
+pr = print
+en = enumerate
+hepu = heapq.heappush
+hepo = heapq.heappop
+bil = bisect_left
+bir = bisect_right
+dedict = defaultdict
+
+# ===== 方向ベクトル(上(負)から時計回り) =====
+DIR4 = ((-1, 0), (0, 1), (1, 0), (0, -1))
+DIR8 = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))
+DIR9 = DIR8 + ((0, 0),) # 中央は最後
+
+# ===== 文字列のリスト =====
+LOWER = list(string.ascii_lowercase) # 小文字 a-z の文字列リスト
+UPPER = list(string.ascii_uppercase) # 大文字 A-Z の文字列リスト
+DIGITS = list(string.digits) # 数字 0-9 の文字列リスト
+
+# ===== よく使う出力関数 =====
+Yes = lambda **kwargs: print("Yes", **kwargs)
+No = lambda **kwargs: print("No", **kwargs)
+yes = lambda **kwargs: print("yes", **kwargs)
+no = lambda **kwargs: print("no", **kwargs)
+YES = lambda **kwargs: print("YES", **kwargs)
+NO = lambda **kwargs: print("NO", **kwargs)
+
+def yn(cond: bool, yes: str = "Yes", no: str = "No", **kwargs) -> None:
+    """条件に応じてYes/No出力"""
+    print(yes if cond else no, **kwargs)
+
+def print_grid(grid: list[list], sep: str = '') -> None:
+    """グリッド表示"""
+    print('\n'.join(sep.join(map(str, row)) for row in grid))
+
+# ===== デバッグ =====
+# AtCoder提出時は自動で無力化
+if "ATCODER" not in os.environ and "ONLINE_JUDGE" not in os.environ:
+    def debug(*args, sep=' ', end='\n', flush=False) -> None:
+        """デバッグ出力（標準エラー）"""
+        print("[DEBUG]", *args, sep=sep, end=end, file=sys.stderr, flush=flush)
+else:
+    def debug(*args, **kwargs) -> None:
+        pass
+
+# ===== template.py =====
+def dijkstra(g: list[list[tuple[int, int]]], s: int) -> list[int]:
+    """概要:
+        非負重みグラフで始点 s からの最短距離を求める。
+    入力:
+        g (list[list[tuple[int, int]]]): 重み付き隣接リスト。
+        s (int): 始点。
+    出力:
+        list[int]: 各頂点への最短距離（未到達は INF）。
+    補足:
+        計算量は O((V+E)logE)。単純グラフなど E = O(V^2) の場合は
+        O((V+E)logV) と書ける。負辺は非対応。
+    """
+    dist = [INF] * len(g)
+    dist[s] = 0
+    pq = [(0, s)]
+    heappop = heapq.heappop
+    heappush = heapq.heappush
+    while pq:
+        d, v = heappop(pq)
+        if d > dist[v]: continue
+        for to, w in g[v]:
+            nd = d + w
+            if nd < dist[to]:
+                dist[to] = nd
+                heappush(pq, (nd, to))
+    return dist
+# ==============================================
+# =================== main =====================
+# ==============================================
+
+def main() -> None:
+    # ここに解答を書く
+    N, Q = MAP()
+    A = LIST()
+    B = LIST()
+    g = [[] for i in range(N+1)]
+    for i in range(N):
+        nxt = (i+1) % N
+        g[i].append((nxt, A[i]))
+        g[nxt].append((i, A[i]))
+        g[i].append((N, B[i]))
+        g[N].append((i, B[i]))
+    d = dijkstra(g, N)
+    As = [0] + list(accumulate(A*2))
+    debug(As)
+    debug(d)
+    ans = []
+    for _ in range(Q):
+        S, T = MAP()
+        ans_tmp = min(min(As[T-1] - As[S-1], As[S+N-1] - As[T-1]) if T != N+1 else INF, d[T-1]+d[S-1])
+        ans.append(ans_tmp)
+    print(*ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
